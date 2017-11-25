ff36167facbe682e324e455593dae330db5d2fbc
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-11-25 17:08:50 +0700

    add scrolling, interview-info, scroll button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { Input, Tabs, Table, Row, Col, Icon, Modal } from 'antd';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from 'react'
+
 import axios from 'axios'
 import _ from 'lodash'
-const Search = Input.Search;
-const TabPane = Tabs.TabPane;
+import Scroll from 'react-scroll'
+import { Input, Tabs, Table, Row, Col, Icon, Modal, Button } from 'antd'
+import logo from './assets/logo.png'
+import './App.css'
+
+const Element = Scroll.Element
+const scroller = Scroll.scroller
+
+const Search = Input.Search
+const TabPane = Tabs.TabPane
 
 const columns = [{
   title: 'รหัสสัมภาษณ์',
@@ -25,11 +31,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchInput: '',
       candidateList: [],
-      filteredList: [],
-      successVisible: false,
-      failureVisible: false
+      filteredList: []
     }
   }
 
@@ -61,16 +64,23 @@ class App extends Component {
       this.setState({ filteredList })
     }
     if (filteredList.length === 0) {
-      this.setState({ failureVisible: true })
+      console.log('FUCK')
+      Modal.error({
+        title: 'Sorry :( ไม่มีชื่ออ่ะครับ',
+        okText: 'close',
+        content: 'ปีหน้ามาสมัครใหม่น้าา'
+      })
     } else {
-      this.setState({ successVisible: true })
+      Modal.success({
+        title: 'Congratulation! ติดค่ายแล้วจ้าา',
+        okText: 'close',
+        content: (
+          <div>
+            <Table columns={columns} dataSource={filteredList} />
+          </div>
+        )
+      })
     }
-  }
-
-  renderSearchResult(){
-    return(
-      <Table columns={columns} dataSource={this.state.filteredList} style={{ margin: 20, padding: 20 }} />
-    )
   }
 
   renderGroup(major){
@@ -96,29 +106,69 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="App-corner">
+          <Row>
+            <Col span={5} />
+            <Col span={5}>
+            <Button ghost size="large" style={{margin: 20}} onClick={() => scroller.scrollTo('information', {
+              duration: 1000,
+              delay: 100,
+              smooth: true,
+              offset: 0
+            })}>รายละเอียด</Button>
+            </Col>
+            <Col span={4}>
             <img src={logo} className="App-logo" alt="logo"/>
-          </div>
+            </Col>
+            <Col span={5}>
+            <Button ghost size="large" style={{ margin: 20 }} onClick={() => scroller.scrollTo('allCandidate', {
+              duration: 1000,
+              delay: 100,
+              smooth: true,
+              offset: 0
+            })}>ดูผลทั้งหมด</Button>
+            </Col>
+          </Row>
           <h1 className="Camp-name">YWC-15th</h1>
-          <h1 className="App-title">อยากรู้ว่าตัวเองติดไหม ใส่ชื่อเลยยยย</h1>
+          <h1 className="App-title"><b>อยากรู้ว่าตัวเองติดไหม ใส่ชื่อเลยยยย</b></h1>
           <Search
             placeholder="ค้นหารายชื่อ"
-            style={{ width: 300, marginTop: 30, height: 40 }}
+            style={{ width: 300, marginTop: 70, height: 40 }}
             onSearch={this.onSubmit}
             size='large'
           />
         </header>
-        <Modal
-          title="Modal"
-          visible={this.state.successVisible}
-          onOk={this.hideModal}
-          onCancel={this.hideModal}
-          okText="close"
-          closable={false}
-          width={700}
-        >
-          {this.renderSearchResult()}
-        </Modal>
+        <div className="App-info">
+          <Element name="information" />
+          <Row style={{marginTop: 20}}>
+            <h1> รายละเอียดการสัมภาษณ์ </h1>
+            <h2>การสัมภาษณ์จะจัดขึ้นใน<b><u>วันที่ 26 พฤศจิกายน 2560 ณ อาคาร ซี.พี.ทาวเวอร์ 1 (สีลม)</u></b><br/>
+            ซึ่งจะแบ่งออกเป็น 2 รอบ คือ <b><u>รอบช่วงเช้าตั้งแต่เวลา 9.00 น. ถึง 12.00 น.</u></b> 
+            และ <b><u>รอบช่วงบ่ายตั้งแต่เวลา 13.00 น. ถึง 18.00 น.</u></b></h2>
+          </Row>
+          
+          <Row style={{ marginTop: 20, textAlign: "left", marginLeft:50, marginRight: 50 }}>
+            <h1> สิ่งที่ต้องเตรียมมาในวันสัมภาษณ์ </h1>
+            <h2>1. บัตรประชาชนสำหรับการแลกบัตรเข้าอาคาร ซี.พี.ทาวเวอร์ 1 (สีลม) และ บัตรนักศึกษาสำหรับการลงทะเบียนสัมภาษณ์ กรุณาแต่งกายด้วยชุดนักศึกษา</h2>
+            <h2>2. การบ้านและสิ่งที่กรรมการสาขากำหนดไว้ กรุณาอ่านรายละเอียดการบ้านและสิ่งที่กรรมการให้เตรียมมาให้ครบถ้วน หากสาขาใดต้องใช้โน้ตบุ๊ค ควรชาร์ตแบตเตอรี่และเตรียมอินเทอร์เน็ตส่วนตัวมาให้พร้อม เนื่องจากสถานที่ไม่มีบริการอินเทอร์เน็ตให้ใช้</h2>
+            <h2>3. Portfolio สามารถนำมาประกอบการสัมภาษณ์ได้ สำหรับน้อง ๆ สาขาดีไซน์จะต้องนำ Portfolio มาด้วยทุกคน</h2>
+          </Row>
+
+          <Row style={{ marginTop: 20, textAlign: "left", marginLeft: 50, marginRight: 50 }}>
+            <Col span={16}>
+              <h1> การเดินทางมาสัมภาษณ์ </h1>
+              <h2>1. ด้วยรถไฟฟ้า BTS สามารถลงสถานีศาลาแดง ณ ทางออกที่ 2</h2>
+              <h2>2. ด้วยรถไฟฟ้า MRT สามารถลงสถานีสีลม ณ ทางออกที่ 2 โดยเดินเรียบทางเท้าไปตามถนนสีลม</h2>
+              <h2>3. ด้วยรถประจำทาง สามารถขึ้นใช้บริการสาย 15, 77, 155, 504, 177, 76</h2>
+            </Col>
+            <Col span={8}>
+              <h1> สอบถามเพิ่มเติมติดต่อ </h1>
+              <h2><b>พี่เบ๊บ:</b> 064-174-7080</h2>
+              <h2><b>พี่ฟง:</b> 092-458-7067</h2>
+              <h2><b>พี่เบนซ์:</b> 085-666-7571</h2>
+            </Col>
+          </Row>
+        </div>
+        <Element name="allCandidate" />
         <Tabs defaultActiveKey="1">
           <TabPane tab={<span><Icon type="file-text" />Content</span>} key="1">{this.renderGroup('content')}</TabPane>
           <TabPane tab={<span><Icon type="edit" />Design</span>} key="2">{this.renderGroup('design')}</TabPane>
