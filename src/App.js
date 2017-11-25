41ffe4b375a3e857b2992f85da7c7519d5b9b0e1
Wasin Watthanasrisong <wasin.w@taskworld.com>
2017-11-25 19:17:31 +0700

    first heroku commit

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
       })
     } else {
       Modal.success({
-        title: 'Congratulation! ติดค่ายแล้วจ้าา',
+        title: 'Congratulation! ติดสัมภาษณ์แล้วจ้าา',
         okText: 'close',
         content: (
           <div>
@@ -120,7 +120,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo"/>
             </Col>
             <Col span={5}>
-            <Button ghost size="large" style={{ margin: 20 }} onClick={() => scroller.scrollTo('allCandidate', {
+            <Button ghost size="large" style={{ margin: 20 }} onClick={() => scroller.scrollTo('allCandidates', {
               duration: 1000,
               delay: 100,
               smooth: true,
@@ -137,44 +137,47 @@ class App extends Component {
             size='large'
           />
         </header>
-        <div className="App-info">
+        <div className="App-info" style={{margin: 50}}>
           <Element name="information" />
-          <Row style={{marginTop: 20}}>
-            <h1> รายละเอียดการสัมภาษณ์ </h1>
+          <Row>
+            <h1 style={{marginBottom: 10}}><b> รายละเอียดการสัมภาษณ์ </b></h1>
             <h2>การสัมภาษณ์จะจัดขึ้นใน<b><u>วันที่ 26 พฤศจิกายน 2560 ณ อาคาร ซี.พี.ทาวเวอร์ 1 (สีลม)</u></b><br/>
             ซึ่งจะแบ่งออกเป็น 2 รอบ คือ <b><u>รอบช่วงเช้าตั้งแต่เวลา 9.00 น. ถึง 12.00 น.</u></b> 
             และ <b><u>รอบช่วงบ่ายตั้งแต่เวลา 13.00 น. ถึง 18.00 น.</u></b></h2>
           </Row>
           
-          <Row style={{ marginTop: 20, textAlign: "left", marginLeft:50, marginRight: 50 }}>
-            <h1> สิ่งที่ต้องเตรียมมาในวันสัมภาษณ์ </h1>
+          <Row style={{ marginTop: 30, textAlign: "left", marginLeft:50, marginRight: 50 }}>
+            <h1 style={{ marginBottom: 10 }}><b> สิ่งที่ต้องเตรียมมาในวันสัมภาษณ์ </b></h1>
             <h2>1. บัตรประชาชนสำหรับการแลกบัตรเข้าอาคาร ซี.พี.ทาวเวอร์ 1 (สีลม) และ บัตรนักศึกษาสำหรับการลงทะเบียนสัมภาษณ์ กรุณาแต่งกายด้วยชุดนักศึกษา</h2>
             <h2>2. การบ้านและสิ่งที่กรรมการสาขากำหนดไว้ กรุณาอ่านรายละเอียดการบ้านและสิ่งที่กรรมการให้เตรียมมาให้ครบถ้วน หากสาขาใดต้องใช้โน้ตบุ๊ค ควรชาร์ตแบตเตอรี่และเตรียมอินเทอร์เน็ตส่วนตัวมาให้พร้อม เนื่องจากสถานที่ไม่มีบริการอินเทอร์เน็ตให้ใช้</h2>
             <h2>3. Portfolio สามารถนำมาประกอบการสัมภาษณ์ได้ สำหรับน้อง ๆ สาขาดีไซน์จะต้องนำ Portfolio มาด้วยทุกคน</h2>
           </Row>
 
-          <Row style={{ marginTop: 20, textAlign: "left", marginLeft: 50, marginRight: 50 }}>
-            <Col span={16}>
-              <h1> การเดินทางมาสัมภาษณ์ </h1>
+          <Row style={{ marginTop: 30, textAlign: "left", marginLeft: 50, marginRight: 50 }}>
+            <Col span={18}>
+              <h1 style={{ marginBottom: 10 }}><b> การเดินทางมาสัมภาษณ์ </b></h1>
               <h2>1. ด้วยรถไฟฟ้า BTS สามารถลงสถานีศาลาแดง ณ ทางออกที่ 2</h2>
               <h2>2. ด้วยรถไฟฟ้า MRT สามารถลงสถานีสีลม ณ ทางออกที่ 2 โดยเดินเรียบทางเท้าไปตามถนนสีลม</h2>
               <h2>3. ด้วยรถประจำทาง สามารถขึ้นใช้บริการสาย 15, 77, 155, 504, 177, 76</h2>
             </Col>
-            <Col span={8}>
-              <h1> สอบถามเพิ่มเติมติดต่อ </h1>
+            <Col span={6}>
+              <h1 style={{ marginBottom: 10 }}><b> สอบถามเพิ่มเติมติดต่อ </b></h1>
               <h2><b>พี่เบ๊บ:</b> 064-174-7080</h2>
               <h2><b>พี่ฟง:</b> 092-458-7067</h2>
               <h2><b>พี่เบนซ์:</b> 085-666-7571</h2>
             </Col>
           </Row>
         </div>
-        <Element name="allCandidate" />
-        <Tabs defaultActiveKey="1">
-          <TabPane tab={<span><Icon type="file-text" />Content</span>} key="1">{this.renderGroup('content')}</TabPane>
-          <TabPane tab={<span><Icon type="edit" />Design</span>} key="2">{this.renderGroup('design')}</TabPane>
-          <TabPane tab={<span><Icon type="area-chart" />Marketing</span>} key="3">{this.renderGroup('marketing')}</TabPane>
-          <TabPane tab={<span><Icon type="desktop" />Programming</span>} key="4">{this.renderGroup('programming')}</TabPane>
-        </Tabs>
+        <div className="App-all-candidates">
+          <Element name="allCandidates" />
+          <h1 style={{ marginBottom: 10 }}><b> รายชื่อผู้ติดสัมภาษณ์ทั้งหมด </b></h1>
+          <Tabs defaultActiveKey="1">
+            <TabPane tab={<span><Icon type="file-text" />Content</span>} key="1">{this.renderGroup('content')}</TabPane>
+            <TabPane tab={<span><Icon type="edit" />Design</span>} key="2">{this.renderGroup('design')}</TabPane>
+            <TabPane tab={<span><Icon type="area-chart" />Marketing</span>} key="3">{this.renderGroup('marketing')}</TabPane>
+            <TabPane tab={<span><Icon type="desktop" />Programming</span>} key="4">{this.renderGroup('programming')}</TabPane>
+          </Tabs>
+        </div>
 
       </div>
     );
